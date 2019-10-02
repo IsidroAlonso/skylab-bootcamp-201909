5fbf2b3b91075839806bb013512a6f286d3779c0
Isidro Alonso <isi_alonso@hotmail.com>
2019-10-02 17:11:30 +0200

    added files

---
+++ staff/isidro-alonso/skylab-semantics/z-sacar-html.js
@@ -0,0 +1,11 @@
+let elems = document.querySelectorAll('h1,address,header,nav,section,aside,footer,main,mark,summary,figure,figcaption,details,article')
+
+console.log(this.document.querySelector('h1 address header nav section aside footer main mark summary figure figcaption details article'));
+
+console.log(this.document.getElementsByTagName(elems)[0]);
+
+let elems4 = this.document.querySelectorAll('h1 address header nav section aside footer main mark summary figure figcaption details article');
+
+this.document.getElementsByTagName(elems4)[0];
+
+const regex = "";
