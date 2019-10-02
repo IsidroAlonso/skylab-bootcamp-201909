cda66b7b185d3e645313f6f2f5d50b91c97ffa49
Isidro Alonso <isi_alonso@hotmail.com>
2019-10-02 22:58:11 +0200

    update files

--- staff/isidro-alonso/skylab-semantics/z-sacar-html.js
+++ staff/isidro-alonso/skylab-semantics/z-sacar-html.js
@@ -1,20 +1,21 @@
 let semanticElements = ['div', 'p', 'a', 'span', 'script', 'strong', 'b', 'noscript', 'br', '#text', '#comment', 'button', 'i'];
-       let text = '';
-       let cont = 0;
-       dom = document.getElementsByTagName('body');
-       dom = dom[0];
-       console.log(dom);
-       hijos(dom);
-       function hijos(padre) {
-           while (padre.hasChildNodes()) {
-               const tagName = padre.firstChild.nodeName.toLowerCase();
-               if (!semanticElements.includes(tagName)) text += ('<' + tagName + '>');
-               if (padre.hasChildNodes()) hijos(padre.firstChild);
-               if (!semanticElements.includes(tagName)) text += ('</' + tagName + '>');
-               padre.removeChild(padre.childNodes[0]);
-           }
-       }
-       localStorage.setItem('SemanticTree', text);
+let text = '';
+dom = document.getElementsByTagName('body');
+dom = dom[0];
+// console.log(dom);
+hijos(dom);
+function hijos(padre) {
+    while (padre.hasChildNodes()) {
+        const tagName = padre.firstChild.nodeName.toLowerCase();
+        if (!semanticElements.includes(tagName)) text += ('<' + tagName + '>');
+        if (padre.hasChildNodes()) hijos(padre.firstChild);
+        if (!semanticElements.includes(tagName)) text += ('</' + tagName + '>');
+        padre.removeChild(padre.childNodes[0]);
+    }
+}
+localStorage.setItem('SemanticTree', text);
+
+console.log(hijos(padre));
 
 // let elems = document.querySelectorAll('h1,address,header,nav,section,aside,footer,main,mark,summary,figure,figcaption,details,article')
 
