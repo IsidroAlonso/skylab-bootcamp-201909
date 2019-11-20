0ae4caf76769e798b508456c8e3d59d5a9564691
Isidro Alonso <isi_alonso@hotmail.com>
2019-11-20 12:10:34 +0100

    update files

--- staff/isidro-alonso/tasks/tasks-api/logic/authenticate-user/index.js
+++ staff/isidro-alonso/tasks/tasks-api/logic/authenticate-user/index.js
@@ -1,6 +1,6 @@
 const validate = require('../../utils/validate')
 const { CredentialsError } = require('../../utils/errors')
-const database = require('../../utils/database')
+const { models: { User } } = require('../../data')
 
 module.exports = function (username, password) {
     validate.string(username)
@@ -8,24 +8,49 @@ module.exports = function (username, password) {
     validate.string(password)
     validate.string.notVoid('password', password)
 
-    const client = database()
+    return User.findOne({ username, password })
+        .then(user => {
+            debugger
+            if (!user) throw new CredentialsError('wrong credentials')
 
-    return client.connect()
-        .then(db => {
-            const users = db.collection('users')
+            // const { _id } = user
+            user.lastAccess = new Date
 
-            return users.findOne({ username, password })
-                .then(user => {
-                    if (!user) throw new CredentialsError('wrong credentials')
+            return user.save()
+                .then(() => id = user.id)
 
-                    const { _id } = user
-
-                    return users.updateOne({ _id }, { $set: { lastAccess: new Date } })
-                        .then(result => {
-                            if (!result.modifiedCount) throw Error('could not update user')
-
-                            return _id.toString()
-                        })
-                })
         })
+
 }
+
+// const validate = require('../../utils/validate')
+// const { CredentialsError } = require('../../utils/errors')
+// const database = require('../../utils/database')
+
+// module.exports = function (username, password) {
+//     validate.string(username)
+//     validate.string.notVoid('username', username)
+//     validate.string(password)
+//     validate.string.notVoid('password', password)
+
+//     const client = database()
+
+//     return client.connect()
+//         .then(db => {
+//             const users = db.collection('users')
+
+//             return users.findOne({ username, password })
+//                 .then(user => {
+//                     if (!user) throw new CredentialsError('wrong credentials')
+
+//                     const { _id } = user
+
+//                     return users.updateOne({ _id }, { $set: { lastAccess: new Date } })
+//                         .then(result => {
+//                             if (!result.modifiedCount) throw Error('could not update user')
+
+//                             return _id.toString()
+//                         })
+//                 })
+//         })
+// }
